3ee7b307cc37c06c286a0e1d65ffe4720deab04d
samuelb2 <aprendizpeoleanalyticse2@gco.com.co>
2025-02-18 17:11:56 -0500

    se crea

--- app/api/models/proveedorLogistica.py
+++ app/api/models/proveedorLogistica.py
@@ -9,3 +9,11 @@ class Proveedor():
     __tablename__='proveedores'
     nombres=Column(String(50))
     documento=Column(String(50))
+    direccion=Column(String(50))
+    ciudad = Column(String(50))
+    representante = Column(String(50))
+    telefono_contacto = Column(String(20))
+    correo = Column(String(100))
+    fecha_envio = Column(Date)
+    costo_envio = Column(Float)
+    descripcion = Column(Text)
